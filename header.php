66d2b7d147c0991c6acf32838cc1b956de3dc09e
g-battaglia <battaglia.giacomo@icloud.com>
2021-06-03 20:39:02 +0200

    Attempt to fix uploading issue.

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
   <meta content="IE=edge" http-equiv="X-UA-Compatible">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <link crossorigin="anonymous" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" rel="stylesheet" />
-  <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/wise.css" rel="stylesheet">
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> <!-- AOS Library -->
 
   <script defer src="<?php echo get_template_directory_uri(); ?>/script.js"></script>
